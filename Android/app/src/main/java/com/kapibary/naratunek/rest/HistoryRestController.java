29f831479feb457c3a44b23ce098e73d0918bef0
Slyboots <michal.blach92@gmail.com>
2017-10-27 19:33:52 +0200

    firebase installed

---
+++ Android/app/src/main/java/com/kapibary/naratunek/rest/HistoryRestController.java
@@ -0,0 +1,32 @@
+package com.kapibary.naratunek.rest;
+
+import android.util.Log;
+
+import com.studioidan.httpagent.HttpAgent;
+import com.studioidan.httpagent.JsonCallback;
+
+import org.json.JSONObject;
+
+
+/**
+ * Created by slyboots on 10/27/17.
+ */
+
+public class HistoryRestController {
+
+
+
+    public static void fetchHistory(){
+        HttpAgent.get("10.99.130.72:8080/api/donors")
+                .goJson(new JsonCallback() {
+
+                    @Override
+                    protected void onDone(boolean b, JSONObject jsonObject) {
+                        Log.d(TAG, jsonObject.toString());
+                    }
+
+                });
+    }
+
+
+}
---
+++ Android/app/src/main/java/com/kapibary/naratunek/rest/RestClient.java
@@ -0,0 +1,27 @@
+package com.kapibary.naratunek.rest;
+
+/**
+ * Created by slyboots on 10/27/17.
+ */
+
+
+
+import com.loopj.android.http.*;
+
+public class RestClient {
+    private static final String BASE_URL = "https://api.twitter.com/1/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
