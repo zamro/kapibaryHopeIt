41f087b560a6e8b0ec79342f205402de7ac6fdbc
zamro <pawel.1.gumny@nokia.com>
2017-10-28 04:05:42 +0200

    Little refactor of MainActivity, added moving into activities from list.

--- Android/app/src/main/java/com/kapibary/naratunek/activity/MainActivity.java
+++ Android/app/src/main/java/com/kapibary/naratunek/activity/MainActivity.java
@@ -1,19 +1,17 @@
 package com.kapibary.naratunek.activity;
 
-import android.app.FragmentManager;
+import android.content.Intent;
 import android.content.res.Configuration;
-import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.FrameLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 
 import com.kapibary.naratunek.R;
 import com.kapibary.naratunek.adapters.MainDrawerListAdapter;
@@ -23,11 +21,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView mDrawerList;
-    RelativeLayout mDrawerPane;
+    private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
-
+    private ActionBar mActionBar;
     ArrayList <NavigationItem> navigationItems;
 
     @Override
@@ -35,43 +32,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        navigationItems = new ArrayList<>();
-        navigationItems.add(new NavigationItem("Uzytkownik", "Lokalizacja", 0));
-        navigationItems.add(new NavigationItem("Wiadomosci", "", 0));
-        navigationItems.add(new NavigationItem("Historia wplat", "", 0));
-        navigationItems.add(new NavigationItem("Ustawienia", "", 0));
-        //navigationItems.add(new NavigationItem("Ustawienia", "", R.drawable.ic_action_about));
+        generateNavigationItems();
 
-        // DrawerLayout
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        // Populate the Navigtion Drawer with options
-        mDrawerPane = (RelativeLayout) findViewById(R.id.content_frame);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        MainDrawerListAdapter adapter = new MainDrawerListAdapter(this, navigationItems);
-        mDrawerList.setAdapter(adapter);
+        mActionBar = getSupportActionBar();
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerList.setAdapter(new MainDrawerListAdapter(this, navigationItems));
+
+        mActionBar.setDisplayHomeAsUpEnabled(true);
         mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.string.navigation_drawer_open,  /* "open drawer" description */
-                R.string.navigation_drawer_close /* "close drawer" description */
+                this,
+                mDrawerLayout,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
         ) {
-
-            /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
                 invalidateOptionsMenu();
-                getSupportActionBar().setTitle("Na Ratunek");
+                mActionBar.setTitle("Na Ratunek");
             }
-
-            /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
                 invalidateOptionsMenu();
-                getSupportActionBar().setTitle("Menu");
+                mActionBar.setTitle("Menu");
             }
         };
         mDrawerToggle.setDrawerIndicatorEnabled(true);
@@ -91,15 +75,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Drawer Item click listeners
-       /* mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 selectItemFromDrawer(position);
             }
-        }); */
-
-
+        });
     }
+
+    private void generateNavigationItems() {
+        navigationItems = new ArrayList<>();
+        navigationItems.add(new NavigationItem("Użytkownik", "Wrocław, Polska", 0)); //todo - localization - dynamic or from registration
+        navigationItems.add(new NavigationItem("Wiadomości", "", 0));
+        navigationItems.add(new NavigationItem("Historia wpłat", "", 0));
+        navigationItems.add(new NavigationItem("Ustawienia", "", 0));
+    }
+
     @Override
     protected void onPostCreate(Bundle savedInstanceState)
     {
@@ -115,22 +106,17 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-        // Handle your other action bar items...
-
         return super.onOptionsItemSelected(item);
     }
 
-
-    /*
-* Called when a particular item from the navigation drawer
-* is selected.
-* */
+    private final Class[] activities = {MainActivity.class, MessagesActivity.class, HistoryActivity.class, TempActivity.class};
     private void selectItemFromDrawer(int position) {
+        Log.d("DRAWER", "" + position);
+        Intent i = new Intent(this, activities[position]);
+        startActivity(i);
         /*Fragment fragment = new PreferencesFragment();
 
         FragmentManager fragmentManager = getFragmentManager();
