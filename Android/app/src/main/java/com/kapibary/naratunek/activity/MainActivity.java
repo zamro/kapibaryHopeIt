89c3a7e762d13a8ecae7c587c205c0b6b0a2e0de
Mariuszw13 <mariuszw570@gmail.com>
2017-10-28 12:49:18 +0200

    main activity style update

--- Android/app/src/main/java/com/kapibary/naratunek/activity/MainActivity.java
+++ Android/app/src/main/java/com/kapibary/naratunek/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
